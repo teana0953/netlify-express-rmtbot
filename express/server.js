ee5de2cd0a9feff0cecd0d35892819aa9f8782eb
Tina <tina@catchyourhealth.com.tw>
2020-01-11 15:57:18 +0800

    test

--- express/server.js
+++ express/server.js
@@ -32,20 +32,16 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const linebotParser = bot.parser();
-app.post('/', linebotParser);
+// app.post('/', linebotParser);
 // const bodyParser = require('body-parser');
 
-// const router = express.Router();
-// router.get('/', (req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/html' });
-//   res.write('<h1>Hello from Express.js!</h1>');
-//   res.end();
-// });
-// router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-// router.post('/', (req, res) => res.json({ postBody: req.body }));
+const router = express.Router();
+// router.post('/', linebotParser);
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 // app.use(bodyParser.json());
-// app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 // app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
